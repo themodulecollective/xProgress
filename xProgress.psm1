4b5bb90b90e042ea373ae7a6565b34ce5af8891c
Mike Campbell <mike@campbellnetwork.net>
2022-10-28 19:12:42 -0400

    add comment based help

--- xProgress.psm1
+++ xProgress.psm1
@@ -2,12 +2,27 @@ $script:ProgressTracker = @{}
 
 Function Write-xProgress
 {
+    <#
+    .SYNOPSIS
+        Writes powershell progress output using Write-Progress based on a previous Initialize-xProgress identity
+    .DESCRIPTION
+        Writes powershell progress output using Write-Progress based on a previous Initialize-xProgress identity
+    .EXAMPLE
+        Write-xProgress -Identity $xProgressID
+        calls Write-Progress with previously defined activity and automatically generated counter, progress, and seconds remaining
+    #>
+
+
     [cmdletbinding()]
     param(
-        [guid]$Identity
+        [guid]$Identity #GUID or GUID string provided from a previously run Initialize-xProgress
     )
 
     $ProgressGUID = $Identity.guid #set the ProgressGUID to the string represenation of the Identity GUID
+    if (-not $Script:ProgressTracker.containsKey($ProgressGUID))
+    {
+        throw("No xProgress Instance found for identity $ProgressGUID")
+    }
     $counter = $Script:ProgressTracker.$($ProgressGUID).Counter++ #advance the counter
     $progressInterval = $Script:ProgressTracker.$($ProgressGUID).ProgressInterval #get the progressInterval for the modulus check
 
@@ -41,29 +56,46 @@ Function Write-xProgress
 
 Function Initialize-xProgress
 {
+    <#
+    .SYNOPSIS
+        Initializes an instance of xProgress for later display using Write-xProgess
+    .DESCRIPTION
+        Initializes an instance of xProgress for later display using Write-xProgess.
+        Automatically sets up counters, timers, and incremental progress tracking.
+        Can show progress only at a selected interval to improve performance (write-progress is expensive).
+    .EXAMPLE
+        $xProgressID = Initialize-xProgress -ArrayToProcess $MyListOfItems -CalculatedProgressInterval 1Percent -Activity "Process MyListOfItems"
+        Sets up xProgress to display progress for a looped operation on $MyListOfItems.  When Write-xProgress is called will update progress at each one percent increment of processing and will use -activity as the activity for Write-Progress.
+    .EXAMPLE
+        $xProgressID = Initialize-xProgress -ArrayToProcess $MyListOfItems -ExplicitProgressInterval 5 -Activity "Process MyListOfItems"
+        Sets up xProgress to display progress for a looped operation on $MyListOfItems.  When Write-xProgress is called will update progress once for each 5 items of processing and will use -activity as the activity for Write-Progress.
+        Will throw an error if MyListOfItems is less than 5 items.
+    #>
+
+
     [cmdletbinding(DefaultParameterSetName = 'CalculatedInterval')]
     param(
         [parameter(Mandatory)]
-        [psobject[]]$ArrayToProcess
+        [psobject[]]$ArrayToProcess #The array of items to be processed
         ,
         [parameter(ParameterSetName = 'CalculatedInterval')]
         [alias('CalculatedInterval','CPI')]
         [ValidateSet('1Percent','10Percent','20Percent','25Percent','Each')]
-        [string]$CalculatedProgressInterval = '1Percent'
+        [string]$CalculatedProgressInterval = '1Percent' #Select a progress interval.  Default is 1 Percent (1Percent).
 
         ,
         [parameter(ParameterSetName = 'ExplicitInterval')]
         [alias('ExplicitInterval','EPI')]
-        [int32]$ExplicitProgressInterval
+        [int32]$ExplicitProgressInterval #specify an explicity item count at which to show progress.
         ,
         [parameter(Mandatory)]
-        [string]$Activity
+        [string]$Activity #displayed in the progress bar Activity field (passed through to Write-Progress -Activity).
         ,
         [parameter()]
-        [int32]$Id
+        [int32]$Id #set the Id for Write-Progress, if desired.
         ,
         [parameter()]
-        [int32]$ParentId
+        [int32]$ParentId #set the ParentId for Write-Progress, if desired.
     )
 
     $ProgressGuid = $(New-Guid).guid
@@ -114,9 +146,23 @@ Function Initialize-xProgress
 
 Function Complete-xProgress
 {
+    <#
+    .SYNOPSIS
+        Completes xProgress for a specific xProgress identity created by Initialize-xProgress
+    .DESCRIPTION
+        Completes xProgress for a specific xProgress identity created by Initialize-xProgress.
+        Removes the progress bar display in Powershell by calling Write-Progress with -Complete parameter.
+        Removes the xProgress identity from xProgress module memory
+    .EXAMPLE
+        Complete-xProgress -Identity $xProgressId
+        removes the progress bar from display and removes the xProgressId from xProgress module memory
+    #>
+
+
     [cmdletbinding()]
     param(
-        [guid]$Identity
+        [parameter(Mandatory)]
+        [guid]$Identity #the xProgress Identity to complete
     )
 
     $ProgressGUID = $Identity.guid #set the ProgressGUID to the string represenation of the Identity GUID
