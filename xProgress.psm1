66990dfa76cb43073d0240598ccca09cab603a5c
Mike Campbell <exactmike@users.noreply.github.com>
2022-10-31 15:16:12 -0400

    add job progress function WIP

--- xProgress.psm1
+++ xProgress.psm1
@@ -191,3 +191,25 @@ Function Complete-xProgress
     #Remove Progress Identity GUID
     $script:ProgressTracker.remove($ProgressGUID)
 }
+
+Function Write-xJobProgress
+{
+        param(
+        [System.Management.Automation.Job[]]$Job
+    )
+ 
+    process {
+        foreach ($j in $Job)
+        {
+                #Extracts the latest progress of the job and writes the progress
+                $jobProgressHistory = $j.ChildJobs[0].Progress
+                $latestProgress = $jobProgressHistory[$jobProgressHistory.Count - 1]
+                $latestPercentComplete = $latestProgress.PercentComplete
+                $latestActivity = $latestProgress.Activity
+                $latestStatus = $latestProgress.StatusDescription
+            
+                #When adding multiple progress bars, a unique ID must be provided. Here I am providing the JobID as this
+                Write-Progress -Id $j.Id -Activity $latestActivity -Status $latestStatus -PercentComplete $latestPercentComplete;
+        }
+    }
+}
